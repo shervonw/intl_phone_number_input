f950b9c1448aca2ce970a185dc5ce5f410f75441
shervonw <w.shervonr@gmail.com>
2023-01-02 21:17:14 -0500

    remove safearea

--- lib/src/widgets/selector_button.dart
+++ lib/src/widgets/selector_button.dart
@@ -156,44 +156,42 @@ class SelectorButton extends StatelessWidget {
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(12), topRight: Radius.circular(12))),
       builder: (BuildContext context) {
-        return SafeArea(
-          child: Stack(children: [
-            GestureDetector(
-              onTap: () => Navigator.pop(context),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: DraggableScrollableSheet(
-                builder: (BuildContext context, ScrollController controller) {
-                  return Directionality(
-                    textDirection: Directionality.of(inheritedContext),
-                    child: Container(
-                      decoration: ShapeDecoration(
-                        color: Theme.of(context).canvasColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            topRight: Radius.circular(12),
-                          ),
+        return Stack(children: [
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: DraggableScrollableSheet(
+              builder: (BuildContext context, ScrollController controller) {
+                return Directionality(
+                  textDirection: Directionality.of(inheritedContext),
+                  child: Container(
+                    decoration: ShapeDecoration(
+                      color: Theme.of(context).canvasColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
                         ),
                       ),
-                      child: CountrySearchListWidget(
-                        countries,
-                        locale,
-                        searchBoxDecoration: searchBoxDecoration,
-                        scrollController: controller,
-                        showFlags: selectorConfig.showFlags,
-                        useEmoji: selectorConfig.useEmoji,
-                        autoFocus: autoFocusSearchField,
-                      ),
                     ),
-                  );
-                },
-              ),
+                    child: CountrySearchListWidget(
+                      countries,
+                      locale,
+                      searchBoxDecoration: searchBoxDecoration,
+                      scrollController: controller,
+                      showFlags: selectorConfig.showFlags,
+                      useEmoji: selectorConfig.useEmoji,
+                      autoFocus: autoFocusSearchField,
+                    ),
+                  ),
+                );
+              },
             ),
-          ]),
-        );
+          ),
+        ]);
       },
     );
   }
